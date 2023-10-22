accf4e9e7218069100da0a9982557c00e6106c92
Your Name <bridcallaghan56@gmail.com>
2023-10-22 22:43:45 +0100

    Download and Install MinGW-w64 to test code

--- MPP week3/Q4.c
+++ MPP week3/Q4.c
@@ -1,12 +1,13 @@
 //Accept two integers and return true (1) if either one is 5 or their sum or difference is 5.
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h> // Include the <stdlib.h> header
 
 int main() {
-    int num1 = 10;
-    int num2 = 5;
+    int num1 = 14;
+    int num2 = 6;
     int sum = num1 + num2;
-    int diff = abs(num1 - num2);
+    int diff = abs(num1 - num2); // Use the abs function
 
     bool result = (num1 == 5 || num2 == 5 || sum == 5 || diff == 5);
 
